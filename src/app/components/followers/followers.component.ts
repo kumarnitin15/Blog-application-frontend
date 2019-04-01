ff5bed99a00e3fe19d78396f57e4965331bf71c6
Nitin Kumar <nk525888@gmail.com>
2019-04-01 22:42:37 +0530

    Added the followers component

---
+++ src/app/components/followers/followers.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { TokenService } from 'src/app/services/token.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-followers',
+  templateUrl: './followers.component.html',
+  styleUrls: ['./followers.component.css']
+})
+export class FollowersComponent implements OnInit {
+
+  followers = [];
+  user: any;
+
+  constructor(private userService: UserService, private tokenService: TokenService, private router: Router) { }
+
+  ngOnInit() {
+    this.GetAllFollowers();
+  }
+
+  GetAllFollowers() {
+    this.userService.getUser(this.tokenService.GetPayload()._id).subscribe(data => {
+      this.user = data.user;
+      this.followers = this.user.followers;
+    });
+  }
+
+  IsFollowing(userId) {
+    for(let i=0; i<this.user.following.length; i++) {
+      if(this.user.following[i]._id === userId)
+      return true;
+    }
+    return false;
+  }
+
+  FollowUser(userId, index) {
+    let followLink = <any>document.getElementsByClassName(String(index))[0];
+    if(followLink.classList.contains('disabled'))
+      return;
+    followLink.classList.add('disabled');
+    followLink.style.color = '#ccebff';
+    setTimeout(() => {
+      this.userService.followUser(userId).subscribe(data => {
+        this.GetAllFollowers();
+      });
+    },1000);
+  }
+
+  OpenProfile(userId) {
+    this.router.navigate(['profile',userId]);
+  }
+
+}
