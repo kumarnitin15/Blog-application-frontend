5824fdf1ef4b176c260e48e2cd977011fa5a85e7
Nitin Kumar <nk525888@gmail.com>
2019-04-07 10:57:05 +0530

    Added the profile-followers component

---
+++ src/app/components/profile-followers/profile-followers.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TokenService } from 'src/app/services/token.service';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-profile-followers',
+  templateUrl: './profile-followers.component.html',
+  styleUrls: ['./profile-followers.component.css']
+})
+export class ProfileFollowersComponent implements OnInit {
+
+  user: any;
+  currUser: any;
+  userId: any;
+  currUserId: any;
+  followers = [];
+
+  constructor(private route: ActivatedRoute, private tokenService: TokenService, private userService: UserService, private router: Router) { }
+
+  ngOnInit() {
+    this.userId = this.route.snapshot.params.userId;
+    this.currUserId = this.tokenService.GetPayload()._id;
+    this.router.events.subscribe(val => {
+      setTimeout(()=>{
+        this.userId = this.route.snapshot.params.userId;
+        this.GetFollowers();
+      },200)
+    });
+    this.GetFollowers();
+  }
+
+  GetFollowers() {
+    this.userService.getUser(this.userId).subscribe(data1 => {
+      this.userService.getUser(this.currUserId).subscribe(data2 => {
+        this.user = data1.user;
+        this.currUser = data2.user;
+        this.followers = this.user.followers;
+      });
+    });
+  }
+
+  IsFollowing(userId) {
+    for(let i=0; i<this.currUser.following.length; i++) {
+      if(this.currUser.following[i]._id === userId)
+      return true;
+    }
+    return false;
+  }
+
+  FollowUser(userId, index) {
+    let followLink = <any>document.getElementsByClassName(String(index))[0];
+    if(followLink.classList.contains('disabled'))
+      return;
+    followLink.classList.add('disabled');
+    followLink.style.color = '#ccebff';
+    setTimeout(() => {
+      this.userService.followUser(userId).subscribe(data => {
+        this.GetFollowers();
+      });
+    },1000);
+  }
+
+  OpenProfile(userId) {
+    this.router.navigate(['profile',userId]);
+  }
+
+}
