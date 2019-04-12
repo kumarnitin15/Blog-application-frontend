ef1cca61f3f7566cd687045fe9f16c7216ebe13a
Nitin Kumar <nk525888@gmail.com>
2019-04-12 19:32:20 +0530

    Changed the profile component completely

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TokenService } from 'src/app/services/token.service';
 import io from 'socket.io-client';
+declare var $ : any;
 
 @Component({
   selector: 'app-profile',
@@ -11,63 +12,72 @@ import io from 'socket.io-client';
 })
 export class ProfileComponent implements OnInit {
 
-  user: any;
   userId: any;
   currUserId: any;
-  blogs = [];
-  followers = [];
-  following = [];
+  user: any;
   currUser: any;
   socket: any;
+  loading = true;
+  profileBlogsData = {
+    blogs: [],
+    profilePic: '',
+    user: this.user
+  };
+  profileFollowersData = {
+    currUser: this.currUser,
+    followers: [],
+    profileUser: this.user
+  };
+  profileFollowingData = {
+    currUser: this.currUser,
+    following: [],
+    profileUser: this.user
+  };
 
   constructor(private router: Router, private userService: UserService, private route: ActivatedRoute, private tokenService: TokenService) {
-    // this.socket = io('http://localhost:3000');
     this.socket = io('https://blogapp-backend.herokuapp.com');
   }
 
   ngOnInit() {
+    (<any>document.querySelector('.profilePicDiv')).style.display = 'none';
+    this.route.params.subscribe(val => {
+      this.loading = true;
+      this.init();
+    })
     this.init();
-    this.GetUser();
   }
 
   init() {
-    this.currUserId = this.tokenService.GetPayload()._id;
     this.userId = this.route.snapshot.params.userId;
-    (<any>document.querySelector('.profilePicDiv')).style.display = 'none';
-    this.router.events.subscribe(val => {
-      location.reload();
-    });
-    let tabs = document.querySelectorAll('.profileMenu .item');
-    let divs = <any>document.querySelectorAll('.divs div');
-    for(let i=0; i<tabs.length; i++) {
-      tabs[i].addEventListener('click', function() {
-        for(let j=0; j<i; j++) {
-          if(tabs[j].classList.contains('active')) {
-            divs[j].style.display = 'none';
-            tabs[j].classList.remove('active');
-          }
-        }
-        for(let j=i+1; j<tabs.length; j++) {
-          if(tabs[j].classList.contains('active')) {
-            divs[j].style.display = 'none';
-            tabs[j].classList.remove('active');
-          }
-        }
-        divs[i].style.display = '';
-        tabs[i].classList.add('active');
-      })
-    }
-  }
-
-  GetUser() {
+    this.currUserId = this.tokenService.GetPayload()._id;
     this.userService.getUser(this.userId).subscribe(data1 => {
       this.userService.getUser(this.currUserId).subscribe(data2 => {
         this.user = data1.user;
         this.currUser = data2.user;
-        this.blogs = this.user.blogs;
-        this.followers = this.user.followers;
-        this.following = this.user.following;
-      })
+
+        const blogsData = {
+          blogs: this.user.blogs,
+          profilePic: this.user.profilePic,
+          user: this.user
+        }
+        this.profileBlogsData = blogsData;
+
+        const followersData = {
+          currUser: this.currUser,
+          followers: this.user.followers,
+          profileUser: this.user
+        };
+        this.profileFollowersData = followersData;
+        
+        const followingData = {
+          currUser: this.currUser,
+          following: this.user.following,
+          profileUser: this.user
+        }
+        this.profileFollowingData = followingData;
+
+        this.loading = false;
+      });
     });
   }
 
@@ -79,22 +89,32 @@ export class ProfileComponent implements OnInit {
     return false;
   }
 
-  Follow() {
+  FollowUser(userId) {
     let followLink = <any>document.querySelector('.followLink');
     if(followLink.classList.contains('disabled'))
       return;
     followLink.classList.add('disabled');
     followLink.style.color = '#ccebff';
     setTimeout(() => {
-      this.userService.followUser(this.userId).subscribe(data => {
-        this.GetUser();
-        let userId = this.userId;
+      this.userService.followUser(userId).subscribe(data => {
         const room_name1 = 'notifications-' + userId;
         this.socket.emit('refresh', room_name1);
         const room_name2 = 'side-' + userId;
         this.socket.emit('refresh', room_name2);
+        this.init();
       });
     },1000);
+  }
+
+  SetDisplay(indexShow, indexHide1, indexHide2) {
+    let profileMenuItems = (<any>document.querySelectorAll('.profileMenuItem'));
+    let clickMenuItems = (<any>document.querySelectorAll('.clickMenuItem'));
+    profileMenuItems[indexShow].style.display = '';
+    profileMenuItems[indexHide1].style.display = 'none';
+    profileMenuItems[indexHide2].style.display = 'none';
+    clickMenuItems[indexShow].classList.add('activeClick');
+    clickMenuItems[indexHide1].classList.remove('activeClick');
+    clickMenuItems[indexHide2].classList.remove('activeClick');
   }
 
 }
